c2d83759d46b77e8a588c53c076e5772ff5ae722
Swarnavo Mukherjee <swarnavomukherjee03@gmail.com>
2024-02-03 22:29:21 +0530

    add team component

--- Frontend/src/components/Team.jsx
+++ Frontend/src/components/Team.jsx
@@ -1,49 +1,129 @@
-import React from 'react';
-import './Team.css';
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "./Team.css";
+import swarnavo from "../assets/team-member1.jpg";
+import mrinmoy from "../assets/Team-member2.jpg";
+import stuti from "../assets/Team-member3.jpg";
+import raj from "../assets/Team-Member4.jpg";
+import { FiAward } from "react-icons/fi";
 
 function Team() {
-    return (
-        <div className='flex gap-5'>
-          
-            <div className='card w-60 h-80 m-4 rounded-tr-[80px] rounded-bl-[80px] rounded-md bg-gradient-to-tl from-green-800 to-teal-800 shadow-2xl shadow-green-950 backdrop-blur-lg backdrop-filter opacity-20'>
-             
-                <div className='w-52 h-32 rounded-tr-[72px] m-5 border-2 border-green-400 rounded-lg'><h1></h1></div>
-                <div className='w-52 m-5 h-8 border-2 border-green-400 rounded-md'></div>
-             
-              
-            </div>
-           
-           
-            <div className='card w-60 h-80 m-4 rounded-tr-[80px] rounded-bl-[80px] rounded-md bg-gradient-to-tl from-green-600 to-teal-600 shadow-2xl shadow-green-950 flex'>
-                <div className='h-36 w-10 m-5 mb-1 border-2 border-green-400 rounded-lg '></div>
-                <div className='w-36 ml-20 h-32 rounded-tr-[72px] absolute mt-5 border-2 border-green-400 rounded-lg'></div>
-                <div className='w-52 m-5 mt-2 mb-2 h-8 border-2 border-green-400 rounded-md'></div>
-                <div className='w-52 ml-5 h-28 rounded-bl-[72px] m-4 mt-2  border-2 border-green-400 rounded-lg'></div>
-           
-              
-           
-            </div>
-            
-            <div className='card w-60 h-80 m-4 rounded-tr-[80px] rounded-bl-[80px] rounded-md bg-gradient-to-tl from-green-800 to-teal-800 shadow-2xl shadow-green-950 backdrop-blur-lg backdrop-filter opacity-20'>
-              
-                <div className='w-52 h-32 rounded-tr-[72px] m-5 border-2 border-green-400 rounded-lg'></div>
-                <div className='w-52 m-5 h-8 border-2 border-green-400 rounded-md'></div>
-              
-              
-            </div>
-           
-            <div className='card w-60 h-80 m-4 rounded-tr-[80px] rounded-bl-[80px] rounded-md bg-gradient-to-tl from-green-600 to-teal-600 shadow-2xl shadow-green-950 '>
-             
-                <div className='w-52 h-32 rounded-tr-[72px] m-5 border-2 border-green-400 rounded-lg'></div>
-                <div className='w-52 m-5 h-8 border-2 border-green-400 rounded-md'></div>
-              
-             
-            
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-8 text-center">Meet Our Team</h1>
+      <div className="flex gap-2">
+        <div className="card w-60 h-80 m-4 flex items-center content-center gap-4 rounded-tr-[80px] rounded-bl-[80px] rounded-md bg-gradient-to-tl from-green-800 to-teal-800 shadow-2xl shadow-green-950 backdrop-blur-lg backdrop-filter">
+          <div className="w-52 h-32 mt-4 rounded-tr-[72px] border-2 border-green-400 rounded-lg">
+            <img
+              src={swarnavo}
+              alt="Team Member 1"
+              className="w-full h-full rounded-tr-[72px] object-cover opacity-100"
+            />
           </div>
-    
-        
+          <div className="w-52 h-6 border-2 border-green-400 rounded-md flex items-center justify-center">
+            <p className="text-l font-semibold text-green-400">
+              Swarnavo Mukherjee
+            </p>
+          </div>
+          <div className=" h-20 w-48">
+          <ul style={{ listStyle: 'none', padding: 0 }}>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <FiAward style={{ marginRight: '8px' }} /> AI/ML Developer
+            </li>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <FiAward style={{ marginRight: '8px' }} /> Cyber Security
+            </li>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <FiAward style={{ marginRight: '8px' }} /> Frontend Developer
+            </li>
+          </ul>
         </div>
-      );
-    }
-    
-    export default Team;
+        </div>
+z
+        <div className="card w-60 h-80 m-4 flex items-center content-center gap-4 rounded-tr-[80px] rounded-bl-[80px] rounded-md bg-gradient-to-tl from-green-800 to-teal-800 shadow-2xl shadow-green-950 backdrop-blur-lg backdrop-filter">
+          <div className="w-52 h-32 mt-4 rounded-tr-[72px] border-2 border-green-400 rounded-lg">
+            <img
+              src={mrinmoy}
+              alt="Team Member 2"
+              className="w-full h-full rounded-tr-[72px] object-cover opacity-100"
+            />
+          </div>
+          <div className="w-52 h-6 border-2 border-green-400 rounded-md  flex items-center justify-center">
+            <p className="text-l font-semibold text-green-400">Mrinmay Das</p>
+          </div>
+          <div className=" h-20 w-48">
+          <ul style={{ listStyle: 'none', padding: 0 }}>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <FiAward style={{ marginRight: '8px' }} /> Frontend Developer
+            </li>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <FiAward style={{ marginRight: '8px' }} /> AI/ML Developer
+            </li>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <FiAward style={{ marginRight: '8px' }} /> App Developer
+            </li>
+          </ul>
+        </div>
+        </div>
+
+        <div className="card w-60 h-80 m-4 flex items-center content-center gap-4 rounded-tr-[80px] rounded-bl-[80px] rounded-md bg-gradient-to-tl from-green-800 to-teal-800 shadow-2xl shadow-green-950 backdrop-blur-lg backdrop-filter">
+          <div className="w-52 h-32 mt-4 rounded-tr-[72px] border-2 border-green-400 rounded-lg">
+            <img
+              src={stuti}
+              alt="Team Member 3"
+              className="w-full h-full rounded-tr-[72px] object-cover opacity-100"
+            />
+          </div>
+          <div className="w-52 h-6 border-2 border-green-400 rounded-md  flex items-center justify-center">
+            <p className="text-l font-semibold text-green-400">
+              Stuti Sinha
+            </p>
+          </div>
+          <div className=" h-20 w-48">
+          <ul style={{ listStyle: 'none', padding: 0 }}>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <FiAward style={{ marginRight: '8px' }} /> Frontend Developer
+            </li>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <FiAward style={{ marginRight: '8px' }} /> UI/UX Designer
+            </li>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <FiAward style={{ marginRight: '8px' }} /> Graphics Designer
+            </li>
+          </ul>
+        </div>
+        </div>
+
+        <div className="card w-60 h-80 m-4 flex items-center content-center gap-4 rounded-tr-[80px] rounded-bl-[80px] rounded-md bg-gradient-to-tl from-green-800 to-teal-800 shadow-2xl shadow-green-950 backdrop-blur-lg backdrop-filter">
+          <div className="w-52 h-32 mt-4 rounded-tr-[72px] border-2 border-green-400 rounded-lg">
+            <img
+              src={raj}
+              alt="Team Member 4"
+              className="w-full h-full rounded-tr-[72px] object-cover opacity-100"
+            />
+          </div>
+          <div className="w-52 h-6 border-2 border-green-400 rounded-md flex items-center justify-center">
+            <p className="text-l font-semibold text-green-400">
+              Raj Bhattacharyya
+            </p>
+          </div>
+          <div className=" h-20 w-48">
+          <ul style={{ listStyle: 'none', padding: 0 }}>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <FiAward style={{ marginRight: '8px' }} /> FullStack Developer
+            </li>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <FiAward style={{ marginRight: '8px' }} /> AI/ML Developer
+            </li>
+            <li style={{ display: 'flex', alignItems: 'center' }}>
+              <FiAward style={{ marginRight: '8px' }} /> App Developer
+            </li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Team;
